d883648731df9b610f0ff099ff62ac77579252b7
venajustin <67924865+venajustin@users.noreply.github.com>
2024-10-15 16:06:20 -0700

    made it work with MSBUILD on my laptop

--- src/testdraw.c
+++ src/testdraw.c
@@ -1,9 +1,9 @@
 #include "stdio.h"
-#include "TermDrawC/termdrawc.h"
+#include "../TermDrawC/termdrawc.h"
 
 int main(int argc, char** argv) {
 
-    printf("hi");
+    printf("hi: %d", addone(6));
 
     return 0;
 }
