5afff5f8822e27fa957d386ce474fe2bfaba0b49
venajustin <venajustin@gmail.com>
2024-10-16 10:53:11 -0700

    fixed error with include

--- src/testdraw.c
+++ src/testdraw.c
@@ -1,9 +1,10 @@
 #include "stdio.h"
-#include "TermDrawC/termdrawc.h"
+#include "termdrawc.h"
 
 int main(int argc, char** argv) {
 
     printf("hi");
+    printf("Math Result: %d", addone(5)); 
 
     return 0;
 }
